5084f04d74776d39180c32043ef3c8bd84a2607f
Ichiro Kawazome <ichiro_k@ca2.so-net.ne.jp>
2025-07-05 14:56:22 +0900

    [add] script files for install linux-6.12.10-zynqmp-fpga-generic-4

---
+++ scripts/install-linux-6.12.10-zynqmp-fpga-generic.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(cd $(dirname $0); pwd)
+REPO_DIR=$(cd $(dirname $0); cd .. ; pwd)
+KERNEL_VERSION=6.12.10
+EXTRA_VERSION=-zynqmp-fpga
+LOCAL_VERSION=-generic
+BUILD_VERSION=4
+
+. "$SCRIPT_DIR/install-variables-zynqmp-fpga.sh"
+. "$SCRIPT_DIR/install-command.sh"
